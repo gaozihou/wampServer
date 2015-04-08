7b738dca7f13ff7abca5a518994862c4585fc704
Paul Gao <gaozihou@sina.com>
2015-04-08 17:33:20 +0800

    Add server test

--- include/DbConnect.php
+++ include/DbConnect.php
@@ -24,7 +24,7 @@ class DbConnect {
         $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
         // Check for database connection error
-        if (mysqli_connect_errno()) {
+        if (\mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
 
@@ -34,4 +34,3 @@ class DbConnect {
 
 }
 
-?>
