2e3a1b1bf860e47a8992d80a3f1182fc05c0ab30
Paul Gao <gaozihou@sina.com>
2015-04-21 20:35:32 +0800

    add post item

--- v1/index.php
+++ v1/index.php
@@ -268,50 +268,8 @@ $app->post('/getSingleItem',function() use($app){
         
 //This is only for testing!!!
 $app->post('/pushMessages', function() use ($app) {
-    
-            $message = $app->request->post('message');
-            $messageToSend = array("Notice" => $message);
-            $registration_ids = array();
-     
-            $db = new DbHandler();
-            $result = $db->getAllGCMIds();     
-            while($row = $result->fetch_assoc()){
-                array_push($registration_ids, $row['registration_id']);
-            }
-            // Set POST variables
-            $url = 'https://android.googleapis.com/gcm/send';
-   
-            $fields = array(
-                'registration_ids' => $registration_ids,
-                'data' => $messageToSend,
-            );
-            $headers = array(
-                'Authorization: key=AIzaSyDdZD8LfeHc3qRHTwgJnXH1ccD4WqiKtNM',
-                'Content-Type: application/json'
-            );
             
-            // Open connection
-             $ch = curl_init();
-   
-            // Set the url, number of POST vars, POST data
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   
-            // Disabling SSL Certificate support temporarly
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-   
-            // Execute post
-            $googleResult = curl_exec($ch);
-            if ($googleResult === FALSE) {
-                die('Curl failed: ' . curl_error($ch));
-            }
-   
-            // Close connection
-            curl_close($ch);
-           
+            $googleResult = pushMessageToAllUsers("hehe");
             // echo json response
             echoRespnse(201, $googleResult);
                 
@@ -703,3 +661,95 @@ function echoRespnse($status_code, $response) {
 }
 
 $app->run();
+
+function pushMessageToAllUsers($rawMessage){
+            $message = $rawMessage;
+            $messageToSend = array("Notice" => $message);
+            $registration_ids = array();
+     
+            $db = new DbHandler();
+            $result = $db->getAllGCMIds();     
+            while($row = $result->fetch_assoc()){
+                array_push($registration_ids, $row['registration_id']);
+            }
+            // Set POST variables
+            $url = 'https://android.googleapis.com/gcm/send';
+   
+            $fields = array(
+                'registration_ids' => $registration_ids,
+                'data' => $messageToSend,
+            );
+            $headers = array(
+                'Authorization: key=AIzaSyDdZD8LfeHc3qRHTwgJnXH1ccD4WqiKtNM',
+                'Content-Type: application/json'
+            );
+            
+            // Open connection
+             $ch = curl_init();
+   
+            // Set the url, number of POST vars, POST data
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   
+            // Disabling SSL Certificate support temporarly
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+   
+            // Execute post
+            $googleResult = curl_exec($ch);
+            if ($googleResult === FALSE) {
+                die('Curl failed: ' . curl_error($ch));
+            }
+   
+            // Close connection
+            curl_close($ch);
+            return $googleResult;
+}
+
+function pushMessageToSingleUser($rawMessage, $user_id){
+            $message = $rawMessage;
+            $messageToSend = array("Notice" => $message);
+            $registration_ids = array();
+     
+            $db = new DbHandler();
+            $result = $db->getUserGCMId($user_id);     
+            while($row = $result->fetch_assoc()){
+                array_push($registration_ids, $row['registration_id']);
+            }
+            // Set POST variables
+            $url = 'https://android.googleapis.com/gcm/send';
+   
+            $fields = array(
+                'registration_ids' => $registration_ids,
+                'data' => $messageToSend,
+            );
+            $headers = array(
+                'Authorization: key=AIzaSyDdZD8LfeHc3qRHTwgJnXH1ccD4WqiKtNM',
+                'Content-Type: application/json'
+            );
+            
+            // Open connection
+             $ch = curl_init();
+   
+            // Set the url, number of POST vars, POST data
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   
+            // Disabling SSL Certificate support temporarly
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+   
+            // Execute post
+            $googleResult = curl_exec($ch);
+            if ($googleResult === FALSE) {
+                die('Curl failed: ' . curl_error($ch));
+            }
+   
+            // Close connection
+            curl_close($ch);    
+            return $googleResult;
+}
